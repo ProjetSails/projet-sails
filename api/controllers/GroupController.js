68c30ec228add5f482832b1f8b195ceef1949d71
Alexandre Plaitant <alexandreplaitant@gmail.com>
2016-10-21 09:51:16 +0200

    Automatisation creation Group/UserRole

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
-	
+
+    create: function (req, res) {
+        Group.create({ name: req.param('name') }).exec(function (err, group) {
+            if (err) { return res.serverError(err); }
+            GroupUserRole.create({user: req.user['id'], group: group['id'], isAdmin: true}).exec(function (err, gur) {
+                if (err) { return res.serverError(err); }
+                return res.ok();
+            });
+        });
+    }
 };
 
